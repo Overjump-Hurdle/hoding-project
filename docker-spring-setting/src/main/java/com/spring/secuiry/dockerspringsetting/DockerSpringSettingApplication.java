2f4e2c95ab265edde9b99d2575ce46cf3864d9a8
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2023-11-10 01:16:57 +0900

    feat: create-project

---
+++ docker-spring-setting/src/main/java/com/spring/secuiry/dockerspringsetting/DockerSpringSettingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.secuiry.dockerspringsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringSettingApplication.class, args);
+    }
+
+}
---
+++ docker-spring-setting/src/test/java/com/spring/secuiry/dockerspringsetting/DockerSpringSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.secuiry.dockerspringsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
